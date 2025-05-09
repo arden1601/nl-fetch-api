0162e91652a89ed87886bf838e54af2a20710db6
arden1601 <corneliusardensatwikahermawan@mail.ugm.ac.id>
2025-05-09 19:51:16 +0700

    feat: add button function for click

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,28 @@
-import Image from "next/image";
+"use client";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function Home() {
-  return (
-    <div>
 
+  const handleClick = () => {
+    const clickFunction = axios.get("/api/dummy")
+    toast.promise(clickFunction, {
+      loading: "Loading...",
+      success: (data) => {
+        return data.data.message
+      },
+      error: (error) => {
+        return error.response.data.message
+      }
+    })
+
+  }
+
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <button className="cursor-pointer" onClick={handleClick}>
+        Click Me
+      </button>
     </div>
   );
 }
